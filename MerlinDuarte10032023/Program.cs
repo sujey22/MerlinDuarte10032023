ddc1948995fe9d1571ca8ca6ca89c2b31c627f0f
sujey22 <da0089032022@unab.edu.sv>
2023-03-11 08:53:14 -0600

    Creando  proyecto

---
+++ MerlinDuarte10032023/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Introduce la altura del triángulo -> ");
+int altura = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= altura; i++)
+{
+    for (int t = 1; t <= i; t++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
